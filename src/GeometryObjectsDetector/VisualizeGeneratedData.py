eec1f0a67c982e26383ea246d610c4f6c1a5b1fe
ayaanmalik04 <ayaannaveed04@gmail.com>
2023-08-04 18:32:12 +0500

    Commit

--- src/GeometryObjectsDetector/VisualizeGeneratedData.py
+++ src/GeometryObjectsDetector/VisualizeGeneratedData.py
@@ -1,6 +1,7 @@
 from src.mrcnn import visualize
 from DataGenerator import generate_geometry_episodes
-
+import os
+from PIL import Image
 class Args:
     def __init__(self):
         self.mask_size = 5
@@ -13,10 +14,19 @@ class Args:
 args = Args()
 
 data_gen, data_gen_val = generate_geometry_episodes(args)
+# Create a folder to store the images
+output_folder = "generated_images"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 for image_id in range(data_gen.last_id):
     image = data_gen.load_image(image_id)
     masks, class_ids = data_gen.load_mask(image_id)
-    #visualize.display_top_masks(image, masks, class_ids, list(data_gen_val.id_name_dic.keys()))
-    visualize.display_images([image], titles=["Image ID: {}".format(image_id)], cols=1)
 
+    # Save the image to the output folder with a unique filename based on the image ID
+    filename = os.path.join(output_folder, "image_{:04d}.png".format(image_id))
+    image_pil = Image.fromarray(image)
+    image_pil.save(filename)
+
+    # You can also display the image if needed
+    # visualize.display_images([image], titles=["Image ID: {}".format(image_id)], cols=1)
